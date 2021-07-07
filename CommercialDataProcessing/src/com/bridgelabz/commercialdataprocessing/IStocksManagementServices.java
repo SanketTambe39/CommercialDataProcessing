5eaed55db1a8c0cd1e127989a18b03d5a5f5e84e
SanketTambe39 <sankettambe39@gmail.com>
2021-07-07 14:50:36 +0530

    ADDED: Buy, Sell and Print details of shares

--- CommercialDataProcessing/src/com/bridgelabz/commercialdataprocessing/IStocksManagementServices.java
+++ CommercialDataProcessing/src/com/bridgelabz/commercialdataprocessing/IStocksManagementServices.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.commercialdataprocessing;
-
-public class IStocksManagementServices {
-
+/**
+ * 
+ * @author Sanket
+ *	This Interface will have abstract methods.
+ * Methods which will be use some operations.
+ * Like Buy new stocks, Sell existing stocks and Print report.
+ */
+public interface IStocksManagementServices {
+		public void displayPresentCompanySharesDetails();
+		public void buyStocks(CompanyShares companySharesDetails);
+		public void sellStocks(String companySymbol);
+		public void displayDetailPortFolio();
 }
